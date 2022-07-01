0973b73cfc54eb68dccf154e65874068c538e717
kochenov <kochenof@gmail.com>
2022-07-01 15:12:39 +0300

    Добавлен макет для страниц с сайдбаром

---
+++ src/views/layouts/pages/RightSidebarLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page wrap">
+    <main class="page-main home">
+      <slot name="content" />
+    </main>
+    <aside class="sidebar">
+      <slot name="sidebar" />
+    </aside>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
