656c425eb7570815f91706f8375f9fd825fa3a60
kochenov <kochenof@gmail.com>
2022-07-05 15:01:51 +0300

    Оптимизировал меню. Требуется рефакторинг

--- src/components/block/navigation/MainMenu.vue
+++ src/components/block/navigation/MainMenu.vue
@@ -1,331 +1,28 @@
 <script setup>
-import { ref } from "vue";
-import { computed, onMounted } from "vue";
-import { useRouter } from "vue-router";
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
 import { useAuth } from "@/stores/authStore";
-const authStore = useAuth();
-const checkLogin = computed(() => authStore.loggedIn && !authStore.loading);
+import { links, subMenu } from "@/components/block/navigation/menuLinks";
 
-const isSubMenu = ref(""); // Название активного Суб Меню
-/**
- * Ссылки главного меню
- * TODO: Переместить в STATE MANAGER
- */
-const links = ref([
-  {
-    id: 1,
-    title: "",
-    subMenu: "home",
-    url: "/",
-    icon: "home",
-    isActive: false,
-  },
-  {
-    id: 2,
-    title: "Новости",
-    subMenu: "news",
-    url: "/news",
-    icon: "rss-square",
-    isActive: false,
-  },
-  {
-    id: 3,
-    title: "О нас",
-    subMenu: "about",
-    url: "/about",
-    icon: "building",
-    isActive: false,
-  },
-  {
-    id: 4,
-    title: "Объявления",
-    subMenu: "bulletinBoard",
-    url: "/bulletin-board",
-    icon: "sticky-note",
-    isActive: "",
-  },
-  {
-    id: 5,
-    title: "Блоги",
-    subMenu: "blogs",
-    url: "/blogs",
-    icon: "blog",
-    isActive: false,
-  },
-  {
-    id: 6,
-    title: "Статьи",
-    subMenu: "articles",
-    url: "/articles",
-    icon: "newspaper",
-    isActive: false,
-  },
-  {
-    id: 7,
-    title: "Расчёты",
-    subMenu: "seeding",
-    url: "/seeding",
-    icon: "calculator",
-    isActive: false,
-  },
-  {
-    id: 8,
-    title: "Календари",
-    subMenu: "сalendars",
-    url: "/сalendars",
-    icon: "calendar-alt",
-    isActive: false,
-  },
-  {
-    id: 9,
-    title: "Кабинет",
-    subMenu: "account",
-    url: "/account",
-    icon: "sign-in-alt",
-    isActive: false,
-  },
-]);
-/**
- * Ссылки суб меню, активное меню указано в meta.parent у роутера
- * TODO: Переместить в STATE MANAGER
- */
-const subMenu = {
-  home: [
-    {
-      id: 1,
-      title: "Главная",
-      url: "/",
-      extrac: true,
-    },
-    {
-      id: 2,
-      title: "Топ Блогеров",
-      url: "/blogers/top",
-    },
-    {
-      id: 3,
-      title: "Топ видео",
-      url: "/video/top",
-    },
-    {
-      id: 4,
-      title: "Сообщеста",
-      url: "/communities",
-    },
-    {
-      id: 5,
-      title: "Стать автором",
-      url: "/authors/register",
-    },
-  ],
-  news: [
-    {
-      id: 1,
-      title: "События",
-      url: "/news/action",
-      extrac: true,
-    },
-    {
-      id: 2,
-      title: "Новости портала",
-      url: "/news",
-    },
-    {
-      id: 3,
-      title: "Новые пользователи",
-      url: "/users/new",
-    },
-    {
-      id: 4,
-      title: "Рекорды",
-      url: "/records",
-    },
-  ],
-  about: [
-    {
-      id: 1,
-      title: "О сайте",
-      url: "/about",
-    },
-    {
-      id: 2,
-      title: "Контакты",
-      url: "/contacts",
-    },
-    {
-      id: 3,
-      title: "Партнёрам",
-      url: "/for-partners",
-    },
-    {
-      id: 4,
-      title: "Авторам",
-      url: "/for-authors",
-    },
-    {
-      id: 5,
-      title: "Стать автором",
-      url: "/author/new",
-    },
-    {
-      id: 6,
-      title: "Обратная связь",
-      url: "/feedback",
-    },
-    {
-      id: 7,
-      title: "Правила",
-      url: "/rules",
-    },
-  ],
-  bulletinBoard: [
-    {
-      id: 1,
-      title: "Недвижимость",
-      url: "/bulletin-board",
-    },
-    {
-      id: 2,
-      title: "Продукция",
-      url: "/production",
-    },
-    {
-      id: 3,
-      title: "Животные",
-      url: "/animals",
-    },
-    {
-      id: 4,
-      title: "Добавить объявления",
-      url: "/board-add",
-    },
-  ],
-  blogs: [
-    {
-      id: 1,
-      title: "Все записи",
-      url: "/posts",
-    },
-    {
-      id: 2,
-      title: "Список блогов",
-      url: "/blogers-list",
-    },
-    {
-      id: 3,
-      title: "Блогеры",
-      url: "/blogers",
-    },
-    {
-      id: 4,
-      title: "Создать блог",
-      url: "/create-add-blog",
-    },
-  ],
-  articles: [
-    {
-      id: 1,
-      title: "Полезное",
-      url: "/articles/useful",
-    },
-    {
-      id: 2,
-      title: "Огород и сад",
-      url: "/articles/vegetable-garden-and-garden",
-    },
-    {
-      id: 3,
-      title: "Животноводство",
-      url: "/articles/animal-husbandry",
-    },
-    {
-      id: 4,
-      title: "Добавить статью",
-      url: "/articles/create",
-    },
-  ],
-  seeding: [
-    {
-      id: 1,
-      title: "Расчёты посева",
-      url: "/calculation/seeding",
-    },
-    {
-      id: 2,
-      title: "Расчёт корма",
-      url: "/calculation/feed",
-    },
-    {
-      id: 3,
-      title: "Контроль веса",
-      url: "/calculation/weight-control",
-    },
-    {
-      id: 4,
-      title: "Бизнес планы",
-      url: "/calculation/bisnes-plans",
-    },
-    {
-      id: 5,
-      title: "Расчёты пользователей",
-      url: "/calculation/all-users",
-    },
-  ],
-  сalendars: [
-    {
-      id: 1,
-      title: "Список задач",
-      url: "/task-list",
-    },
-    {
-      id: 2,
-      title: "Рекомендации",
-      url: "/recomendations",
-    },
-    {
-      id: 3,
-      title: "Задачи пользователей",
-      url: "/user-tasks",
-    },
-    {
-      id: 4,
-      title: "Добавить задачу",
-      url: "/add-task",
-    },
-  ],
-  account: [
-    {
-      id: 1,
-      title: "Вход",
-      url: "/login",
-      logIn: false,
-    },
-    {
-      id: 2,
-      title: "Регистрация",
-      url: "/registration",
-      logIn: false,
-    },
-    {
-      id: 3,
-      title: "Личный кабинет",
-      url: "/dashboard",
-      logIn: true,
-    },
-    {
-      id: 4,
-      title: "Уведомления",
-      url: "/notifications",
-      logIn: true,
-    },
-    {
-      id: 5,
-      title: "Выход",
-      url: "/logout",
-      logIn: true,
-    },
-  ],
+const route = useRoute();
+const authStore = useAuth();
+const checkLogin = computed(() => authStore.loggedIn);
+const checkRoute = () => {
+  if (isSubMenu.value === "" && typeof route.meta.parent !== "undefined") {
+    if (typeof subMenu[route.meta.parent] !== "undefined") {
+      isSubMenu.value = route.meta.parent;
+    }
+  }
 };
+const activeSubMenu = computed(() => {
+  checkRoute();
+  if (subMenu[isSubMenu.value]) {
+    return subMenu[isSubMenu.value].filter(
+      (v) => v.logIn === checkLogin.value || typeof v.logIn == "undefined"
+    );
+  }
+});
+const isSubMenu = ref(""); // Название активного Суб Меню
 
 /**
  * Активация пункта меню,
@@ -341,20 +38,6 @@ const addActiveClass = (id) => {
       value.isActive = false;
     }
   });
-};
-/**
- * Показывает на основе главного активного пункта меню
- * Суб меню
- * @param {*} r $router
- * @return Возвращает массив обьектов активного субменю
- * @todo Оптимизировать
- */
-const subMenuActive = (r) => {
-  if (typeof r !== "undefined" && isSubMenu.value === "") {
-    return subMenu[r.meta.parent];
-  } else {
-    return subMenu[isSubMenu.value];
-  }
 };
 </script>
 <template>
@@ -377,12 +60,8 @@ const subMenuActive = (r) => {
     </ul>
     <div id="sub-menu">
       <ul class="nav__sub-menu">
-        <li v-for="(value, index) in subMenuActive($route)" :key="index">
-          <router-link
-            v-if="value.logIn === checkLogin || typeof value.logIn == 'undefined'"
-            :to="value.url"
-            active-class="active"
-          >
+        <li v-for="(value, index) in activeSubMenu" :key="index">
+          <router-link :to="value.url" active-class="active">
             {{ value.title }}
           </router-link>
         </li>
