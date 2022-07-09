83134180794713df7004db0a7d14b0fcbd4405b0
kochenov <kochenof@gmail.com>
2022-07-09 10:25:24 +0300

    Add Pinia State Manager

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
 
+import App from "./App.vue";
 import router from "@/router";
+
+const pinia = createPinia();
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
 // Импорт иконок
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fas);
 
-createApp(App).use(router).component("fa", FontAwesomeIcon).mount("#app");
+createApp(App)
+  .use(router)
+  .use(pinia)
+  .component("fa", FontAwesomeIcon)
+  .mount("#app");
