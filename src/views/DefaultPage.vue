e2f7dc264629cf7f9396188f90db5f82599edab0
kochenov <kochenof@gmail.com>
2022-07-09 10:28:29 +0300

    add default page and 404 page

--- src/views/DefaultPage.vue
+++ src/views/DefaultPage.vue
@@ -1,12 +1,24 @@
 <template>
-    <div>
-        456
-    </div>
+  <RightSidebarLayout>
+    <template v-slot:content>
+      <TabsWrap
+        :currentComponent="isComponent"
+        :boxComponents="[{ title: $route.title }]"
+      />
+    </template>
+    <template v-slot:sidebar> Это сидебар </template>
+  </RightSidebarLayout>
 </template>
 
 <script setup>
+import { ref, onMounted, markRaw } from "vue";
+import { useRoute } from "vue-router";
 
+import TabsWrap from "@/components/block/tabs/TabsWrap.vue";
+import RightSidebarLayout from "./layouts/pages/RightSidebarLayout.vue";
+const route = markRaw(useRoute());
+const isComponent = ref("NameComponent 1");
+const metaInfo = ref(null);
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        404
-    </div>
+  <RightSidebarLayout>
+    <template v-slot:content>
+      <TabsWrap
+        :boxComponents="[
+          {
+            title: 'Ошибка 404',
+          },
+        ]"
+      >
+        <p>Страница, которую вы ищете, здесь нет.</p>
+      </TabsWrap>
+    </template>
+  </RightSidebarLayout>
 </template>
 
 <script setup>
-
+import RightSidebarLayout from "@/views/layouts/pages/RightSidebarLayout.vue";
+import TabsWrap from "@/components/block/tabs/TabsWrap.vue";
 </script>
 
 <style lang="scss" scoped>
+p {
+  font-size: 24px;
+}
 </style>
